f22f1b9619a93cc390aad63c121c677250e11759
dohyeon <hyeon1018@live.com>
2018-11-24 15:08:17 +0900

    add Category sample data script.

---
+++ db-script/sampledata.sql
@@ -0,0 +1,24 @@
+-- Category
+insert into Category values ("가전");
+insert into Category values ("가구");
+insert into Category values ("도서");
+insert into Category values ("의류");
+insert into Category values ("잡화");
+
+-- User
+
+-- Address
+
+-- Account
+
+-- Favorite
+
+-- Card
+
+-- Item
+
+-- Image
+
+-- Bid
+
+-- Deal

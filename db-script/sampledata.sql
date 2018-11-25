765fd0adadca94647593c2400d680157609264fa
dohyeon <hyeon1018@live.com>
2018-11-26 01:33:07 +0900

    add User and Address Sampledata script
    샘플데이타 2 - User & Address

--- db-script/sampledata.sql
+++ db-script/sampledata.sql
@@ -6,8 +6,16 @@ insert into Category values ("의류");
 insert into Category values ("잡화");
 
 -- User
+insert into User values("abc123", "qwerty", "김도현", "24", "Male", "01012345678");
+insert into User values("tjwldnjs", "123456", "서지원", "24", "Male", "01092275415");
+insert into User values("rnlcksgdk", "000000", "김은희", "28", "Female", "01037825853");
 
 -- Address
+insert into Address values("abs123", "집", "", "01717", "서울시 노원구", "대림아파트");
+insert into Address values("abs123", "학교", "", "05029", "서울시 광진구", "능동로 120 건국대학교");
+insert into Address values("tjwldnjs", "기숙사", "", "05029", "서울시 광진구", "능동로 120 건국대학교");
+insert into Address values("rnlcksgdk", "집", "", "05008", "서울시 광진구", "아차산로 355 아크로텔");
+
 
 -- Account
 

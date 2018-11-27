6961d14bba4c3621f2a7ce4f0b8e0c36302788e7
dohyeon <hyeon1018@live.com>
2018-11-27 15:10:54 +0900

    change login menu directory in package
    GUI 파일들은 모두 gui 패키지에 넣어놔 주세요.

--- src/main/java/com/db/auction/Main.java
+++ src/main/java/com/db/auction/Main.java
@@ -1,20 +1,10 @@
 package com.db.auction;
 
-import java.sql.*;
+import com.db.auction.gui.*;
+
 
 class Main {
-    public static void main( String[] args ) throws SQLException {
-        //create connection for a server installed in localhost, with a user "root" with no password
-        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost/", "root", "mysql")) {
-            // create a Statement
-            try (Statement stmt = conn.createStatement()) {
-                //execute query
-                try (ResultSet rs = stmt.executeQuery("SELECT 'Hello World!'")) {
-                    //position result to first
-                    rs.first();
-                    System.out.println(rs.getString(1)); //result is "Hello World!";
-                }
-            }
-        }
+    public static void main( String[] args ){
+        new LoginMenu();
     }
 }

d9a19ab9e0498c5f8ad29de2a66b1c0f7de12b3d
정유환 <wind9481@naver.com>
2018-11-28 15:14:17 +0900

    Split gui file

---
+++ src/main/java/com/db/auction/gui/AccountInfo.java
@@ -0,0 +1,220 @@
+package com.db.auction.gui;
+
+import com.db.auction.Database;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class AccountInfo {
+
+    private void createButtonActionPerformed(ActionEvent e) {
+        String userId = cInputId.getText();
+        String userPw = String.valueOf(cInputPw.getPassword());
+        String name = cInputName.getText();
+        int age = Integer.parseInt(cInputAge.getText());
+        String gender = genderBtnGroup.getSelection().getActionCommand();
+        String phoneNumber = cInputPhone.getText();
+        String address = cInputAddr.getText();
+
+        if(userId.length() > 0 && userPw.length() > 0 && name.length() > 0 && phoneNumber.length() > 0 && address.length() > 0) {
+            Database.regiesterUser(userId, userPw, name, age, gender, phoneNumber);
+            Database.addAddress(userId, "기본배송지", address);
+
+            JOptionPane.showMessageDialog(regDialog, "User created", "Complete", JOptionPane.INFORMATION_MESSAGE);
+        }else{
+            JOptionPane.showMessageDialog(regDialog, "Please check input", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        // TODO 계정 생성 시 수행해야 하는 것
+    } //button 값은 genderBtnGroup.getSelection().getActionCommand() // Male, Female 출력
+
+    private void regDialogWindowClosing(WindowEvent e) {
+        loginButton.setEnabled(true);
+        regDialog.dispose();
+        e.getWindow().dispose();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Yu Hwan Jung
+        regDialog = new JDialog();
+        regPanel = new JPanel();
+        cIdLabel = new JLabel();
+        cInputId = new JTextField();
+        cPwLabel = new JLabel();
+        cInputPw = new JPasswordField();
+        cNameLabel = new JLabel();
+        cInputName = new JTextField();
+        cAgeLabel = new JLabel();
+        cInputAge = new JTextField();
+        cGenderLabel = new JLabel();
+        cSelectMale = new JRadioButton();
+        cSelectFemale = new JRadioButton();
+        cPhoneLabel = new JLabel();
+        cInputPhone = new JTextField();
+        cAddrLabel = new JLabel();
+        createButton = new JButton();
+        AddrScroll = new JScrollPane();
+        cInputAddr = new JTextArea();
+        genderBtnGroup = new ButtonGroup();
+
+        //======== regDialog ========
+        {
+            regDialog.setTitle("Register");
+            regDialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+            regDialog.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    regDialogWindowClosing(e);
+                }
+            });
+            Container regDialogContentPane = regDialog.getContentPane();
+            regDialogContentPane.setLayout(new BorderLayout());
+
+            //======== regPanel ========
+            {
+                regPanel.setPreferredSize(new Dimension(500, 320));
+
+                // JFormDesigner evaluation mark
+                regPanel.setBorder(new javax.swing.border.CompoundBorder(
+                    new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+                        "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+                        javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+                        java.awt.Color.red), regPanel.getBorder())); regPanel.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+
+                regPanel.setLayout(null);
+
+                //---- cIdLabel ----
+                cIdLabel.setText("ID");
+                cIdLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
+                regPanel.add(cIdLabel);
+                cIdLabel.setBounds(new Rectangle(new Point(40, 30), cIdLabel.getPreferredSize()));
+
+                //---- cInputId ----
+                cInputId.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 18));
+                regPanel.add(cInputId);
+                cInputId.setBounds(95, 30, 120, 30);
+
+                //---- cPwLabel ----
+                cPwLabel.setText("PW");
+                cPwLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
+                regPanel.add(cPwLabel);
+                cPwLabel.setBounds(new Rectangle(new Point(245, 30), cPwLabel.getPreferredSize()));
+
+                //---- cInputPw ----
+                cInputPw.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 18));
+                regPanel.add(cInputPw);
+                cInputPw.setBounds(300, 30, 120, 30);
+
+                //---- cNameLabel ----
+                cNameLabel.setText("\uc774\ub984");
+                cNameLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
+                regPanel.add(cNameLabel);
+                cNameLabel.setBounds(new Rectangle(new Point(40, 75), cNameLabel.getPreferredSize()));
+
+                //---- cInputName ----
+                cInputName.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 18));
+                regPanel.add(cInputName);
+                cInputName.setBounds(95, 75, 120, 30);
+
+                //---- cAgeLabel ----
+                cAgeLabel.setText("\ub098\uc774");
+                cAgeLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
+                regPanel.add(cAgeLabel);
+                cAgeLabel.setBounds(new Rectangle(new Point(245, 75), cAgeLabel.getPreferredSize()));
+
+                //---- cInputAge ----
+                cInputAge.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 18));
+                regPanel.add(cInputAge);
+                cInputAge.setBounds(300, 75, 50, 30);
+
+                //---- cGenderLabel ----
+                cGenderLabel.setText("\uc131\ubcc4");
+                cGenderLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
+                regPanel.add(cGenderLabel);
+                cGenderLabel.setBounds(new Rectangle(new Point(40, 120), cGenderLabel.getPreferredSize()));
+
+                //---- cSelectMale ----
+                cSelectMale.setText("\ub0a8\uc131");
+                cSelectMale.setActionCommand("Male");
+                cSelectMale.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 14));
+                regPanel.add(cSelectMale);
+                cSelectMale.setBounds(new Rectangle(new Point(90, 120), cSelectMale.getPreferredSize()));
+
+                //---- cSelectFemale ----
+                cSelectFemale.setText("\uc5ec\uc131");
+                cSelectFemale.setActionCommand("Female");
+                cSelectFemale.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 14));
+                regPanel.add(cSelectFemale);
+                cSelectFemale.setBounds(new Rectangle(new Point(145, 120), cSelectFemale.getPreferredSize()));
+
+                //---- cPhoneLabel ----
+                cPhoneLabel.setText("\uc804\ud654\ubc88\ud638");
+                cPhoneLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
+                regPanel.add(cPhoneLabel);
+                cPhoneLabel.setBounds(new Rectangle(new Point(210, 120), cPhoneLabel.getPreferredSize()));
+
+                //---- cInputPhone ----
+                cInputPhone.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 18));
+                regPanel.add(cInputPhone);
+                cInputPhone.setBounds(300, 120, 160, 30);
+
+                //---- cAddrLabel ----
+                cAddrLabel.setText("\uc8fc\uc18c");
+                cAddrLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
+                regPanel.add(cAddrLabel);
+                cAddrLabel.setBounds(new Rectangle(new Point(40, 165), cAddrLabel.getPreferredSize()));
+
+                //---- createButton ----
+                createButton.setText("\ub4f1\ub85d");
+                createButton.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
+                createButton.addActionListener(e -> createButtonActionPerformed(e));
+                regPanel.add(createButton);
+                createButton.setBounds(new Rectangle(new Point(220, 235), createButton.getPreferredSize()));
+
+                //======== AddrScroll ========
+                {
+
+                    //---- cInputAddr ----
+                    cInputAddr.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 18));
+                    AddrScroll.setViewportView(cInputAddr);
+                }
+                regPanel.add(AddrScroll);
+                AddrScroll.setBounds(95, 170, 365, 50);
+            }
+            regDialogContentPane.add(regPanel, BorderLayout.CENTER);
+            regDialog.pack();
+            regDialog.setLocationRelativeTo(regDialog.getOwner());
+        }
+
+        //---- genderBtnGroup ----
+        genderBtnGroup.add(cSelectMale);
+        genderBtnGroup.add(cSelectFemale);
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Yu Hwan Jung
+    private JDialog regDialog;
+    private JPanel regPanel;
+    private JLabel cIdLabel;
+    private JTextField cInputId;
+    private JLabel cPwLabel;
+    private JPasswordField cInputPw;
+    private JLabel cNameLabel;
+    private JTextField cInputName;
+    private JLabel cAgeLabel;
+    private JTextField cInputAge;
+    private JLabel cGenderLabel;
+    private JRadioButton cSelectMale;
+    private JRadioButton cSelectFemale;
+    private JLabel cPhoneLabel;
+    private JTextField cInputPhone;
+    private JLabel cAddrLabel;
+    private JButton createButton;
+    private JScrollPane AddrScroll;
+    private JTextArea cInputAddr;
+    private ButtonGroup genderBtnGroup;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
--- src/main/java/com/db/auction/gui/LoginMenu.java
+++ src/main/java/com/db/auction/gui/LoginMenu.java
@@ -43,11 +43,10 @@ public class LoginMenu {
         // TODO 로그인 버튼 입력
         // TODO JTextField는 .getText(), JPasswordField는 String.Valueof(inputPw.getPassword())
         if(Database.isValidUser(inputId.getText(), String.valueOf(inputPw.getPassword()))){
-               msgLabel.setText("Login\nUser name : " + inputId.getText());
+            JOptionPane.showMessageDialog(mainFrame, "Login User name: " + inputId.getText(), "Login", JOptionPane.INFORMATION_MESSAGE);
         }else{
-            msgLabel.setText("Wrong User");
+            JOptionPane.showMessageDialog(mainFrame, "Wrong User", "Login", JOptionPane.ERROR_MESSAGE);
         }
-        msgDialog.setVisible(true);
     }
 
     private void inputIdKeyTyped(KeyEvent e) {
@@ -62,35 +61,6 @@ public class LoginMenu {
         }
     } // TODO 25자 이상 입력 못하도록 함
 
-    private void createButtonActionPerformed(ActionEvent e) {
-        String userId = cInputId.getText();
-        String userPw = String.valueOf(cInputPw.getPassword());
-        String name = cInputName.getText();
-        int age = Integer.parseInt(cInputAge.getText());
-        String gender = genderBtnGroup.getSelection().getActionCommand();
-        String phoneNumber = cInputPhone.getText();
-        String address = cInputAddr.getText();
-
-        if(userId.length() > 0 && userPw.length() > 0 && name.length() > 0 && phoneNumber.length() > 0 && address.length() > 0) {
-            Database.regiesterUser(userId, userPw, name, age, gender, phoneNumber);
-            Database.addAddress(userId, "기본배송지", address);
-
-            msgLabel.setText("create User");
-        }else{
-            msgLabel.setText("Please check input");
-        }
-
-
-        msgDialog.setVisible(true);
-        // TODO 계정 생성 시 수행해야 하는 것
-    } //button 값은 genderBtnGroup.getSelection().getActionCommand() // Male, Female 출력
-
-    private void regDialogWindowClosing(WindowEvent e) {
-        loginButton.setEnabled(true);
-        regDialog.setVisible(false);
-        e.getWindow().dispose();
-    }
-
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - Yu Hwan Jung
@@ -102,28 +72,6 @@ public class LoginMenu {
         inputPw = new JPasswordField();
         loginButton = new JButton();
         registerLabel = new JLabel();
-        regDialog = new JDialog();
-        regPanel = new JPanel();
-        cIdLabel = new JLabel();
-        cInputId = new JTextField();
-        cPwLabel = new JLabel();
-        cInputPw = new JPasswordField();
-        cNameLabel = new JLabel();
-        cInputName = new JTextField();
-        cAgeLabel = new JLabel();
-        cInputAge = new JTextField();
-        cGenderLabel = new JLabel();
-        cSelectMale = new JRadioButton();
-        cSelectFemale = new JRadioButton();
-        cPhoneLabel = new JLabel();
-        cInputPhone = new JTextField();
-        cAddrLabel = new JLabel();
-        createButton = new JButton();
-        AddrScroll = new JScrollPane();
-        cInputAddr = new JTextArea();
-        msgDialog = new JDialog();
-        msgPanel = new JPanel();
-        msgLabel = new JLabel();
 
         //======== mainFrame ========
         {
@@ -211,187 +159,6 @@ public class LoginMenu {
             mainFrame.pack();
             mainFrame.setLocationRelativeTo(mainFrame.getOwner());
         }
-
-        //======== regDialog ========
-        {
-            regDialog.setTitle("Register");
-            regDialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-            regDialog.addWindowListener(new WindowAdapter() {
-                @Override
-                public void windowClosing(WindowEvent e) {
-                    regDialogWindowClosing(e);
-                }
-            });
-            Container regDialogContentPane = regDialog.getContentPane();
-            regDialogContentPane.setLayout(new BorderLayout());
-
-            //======== regPanel ========
-            {
-                regPanel.setPreferredSize(new Dimension(500, 320));
-
-                // JFormDesigner evaluation mark
-                regPanel.setBorder(new javax.swing.border.CompoundBorder(
-                    new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-                        "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
-                        javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-                        java.awt.Color.red), regPanel.getBorder())); regPanel.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
-
-                regPanel.setLayout(null);
-
-                //---- cIdLabel ----
-                cIdLabel.setText("ID");
-                cIdLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
-                regPanel.add(cIdLabel);
-                cIdLabel.setBounds(new Rectangle(new Point(40, 30), cIdLabel.getPreferredSize()));
-
-                //---- cInputId ----
-                cInputId.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 18));
-                regPanel.add(cInputId);
-                cInputId.setBounds(95, 30, 120, 30);
-
-                //---- cPwLabel ----
-                cPwLabel.setText("PW");
-                cPwLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
-                regPanel.add(cPwLabel);
-                cPwLabel.setBounds(new Rectangle(new Point(245, 30), cPwLabel.getPreferredSize()));
-
-                //---- cInputPw ----
-                cInputPw.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 18));
-                regPanel.add(cInputPw);
-                cInputPw.setBounds(300, 30, 120, 30);
-
-                //---- cNameLabel ----
-                cNameLabel.setText("\uc774\ub984");
-                cNameLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
-                regPanel.add(cNameLabel);
-                cNameLabel.setBounds(new Rectangle(new Point(40, 75), cNameLabel.getPreferredSize()));
-
-                //---- cInputName ----
-                cInputName.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 18));
-                regPanel.add(cInputName);
-                cInputName.setBounds(95, 75, 120, 30);
-
-                //---- cAgeLabel ----
-                cAgeLabel.setText("\ub098\uc774");
-                cAgeLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
-                regPanel.add(cAgeLabel);
-                cAgeLabel.setBounds(new Rectangle(new Point(245, 75), cAgeLabel.getPreferredSize()));
-
-                //---- cInputAge ----
-                cInputAge.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 18));
-                regPanel.add(cInputAge);
-                cInputAge.setBounds(300, 75, 50, 30);
-
-                //---- cGenderLabel ----
-                cGenderLabel.setText("\uc131\ubcc4");
-                cGenderLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
-                regPanel.add(cGenderLabel);
-                cGenderLabel.setBounds(new Rectangle(new Point(40, 120), cGenderLabel.getPreferredSize()));
-
-                //---- cSelectMale ----
-                cSelectMale.setText("\ub0a8\uc131");
-                cSelectMale.setActionCommand("Male");
-                cSelectMale.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 14));
-                regPanel.add(cSelectMale);
-                cSelectMale.setBounds(new Rectangle(new Point(90, 120), cSelectMale.getPreferredSize()));
-
-                //---- cSelectFemale ----
-                cSelectFemale.setText("\uc5ec\uc131");
-                cSelectFemale.setActionCommand("Female");
-                cSelectFemale.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 14));
-                regPanel.add(cSelectFemale);
-                cSelectFemale.setBounds(new Rectangle(new Point(145, 120), cSelectFemale.getPreferredSize()));
-
-                //---- cPhoneLabel ----
-                cPhoneLabel.setText("\uc804\ud654\ubc88\ud638");
-                cPhoneLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
-                regPanel.add(cPhoneLabel);
-                cPhoneLabel.setBounds(new Rectangle(new Point(210, 120), cPhoneLabel.getPreferredSize()));
-
-                //---- cInputPhone ----
-                cInputPhone.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 18));
-                regPanel.add(cInputPhone);
-                cInputPhone.setBounds(300, 120, 160, 30);
-
-                //---- cAddrLabel ----
-                cAddrLabel.setText("\uc8fc\uc18c");
-                cAddrLabel.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
-                regPanel.add(cAddrLabel);
-                cAddrLabel.setBounds(new Rectangle(new Point(40, 165), cAddrLabel.getPreferredSize()));
-
-                //---- createButton ----
-                createButton.setText("\ub4f1\ub85d");
-                createButton.setFont(new Font("\ub098\ub214\uace0\ub515", Font.BOLD, 20));
-                createButton.addActionListener(e -> createButtonActionPerformed(e));
-                regPanel.add(createButton);
-                createButton.setBounds(new Rectangle(new Point(220, 235), createButton.getPreferredSize()));
-
-                //======== AddrScroll ========
-                {
-
-                    //---- cInputAddr ----
-                    cInputAddr.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 18));
-                    AddrScroll.setViewportView(cInputAddr);
-                }
-                regPanel.add(AddrScroll);
-                AddrScroll.setBounds(95, 170, 365, 50);
-            }
-            regDialogContentPane.add(regPanel, BorderLayout.CENTER);
-            regDialog.pack();
-            regDialog.setLocationRelativeTo(regDialog.getOwner());
-        }
-
-        //======== msgDialog ========
-        {
-            msgDialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-            msgDialog.setTitle("Message");
-            msgDialog.setModalityType(Dialog.ModalityType.DOCUMENT_MODAL);
-            Container msgDialogContentPane = msgDialog.getContentPane();
-            msgDialogContentPane.setLayout(new BorderLayout());
-
-            //======== msgPanel ========
-            {
-                msgPanel.setPreferredSize(new Dimension(300, 150));
-
-                // JFormDesigner evaluation mark
-                msgPanel.setBorder(new javax.swing.border.CompoundBorder(
-                    new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-                        "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
-                        javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-                        java.awt.Color.red), msgPanel.getBorder())); msgPanel.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
-
-                msgPanel.setLayout(null);
-
-                //---- msgLabel ----
-                msgLabel.setText("message");
-                msgLabel.setFont(new Font("\uad74\ub9bc", Font.PLAIN, 16));
-                msgLabel.setHorizontalAlignment(SwingConstants.CENTER);
-                msgPanel.add(msgLabel);
-                msgLabel.setBounds(0, 20, 300, 70);
-
-                { // compute preferred size
-                    Dimension preferredSize = new Dimension();
-                    for(int i = 0; i < msgPanel.getComponentCount(); i++) {
-                        Rectangle bounds = msgPanel.getComponent(i).getBounds();
-                        preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
-                        preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
-                    }
-                    Insets insets = msgPanel.getInsets();
-                    preferredSize.width += insets.right;
-                    preferredSize.height += insets.bottom;
-                    msgPanel.setMinimumSize(preferredSize);
-                    msgPanel.setPreferredSize(preferredSize);
-                }
-            }
-            msgDialogContentPane.add(msgPanel, BorderLayout.CENTER);
-            msgDialog.pack();
-            msgDialog.setLocationRelativeTo(msgDialog.getOwner());
-        }
-
-        //---- genderBtnGroup ----
-        genderBtnGroup = new ButtonGroup();
-        genderBtnGroup.add(cSelectMale);
-        genderBtnGroup.add(cSelectFemale);
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
@@ -405,28 +172,5 @@ public class LoginMenu {
     private JPasswordField inputPw;
     private JButton loginButton;
     private JLabel registerLabel;
-    private JDialog regDialog;
-    private JPanel regPanel;
-    private JLabel cIdLabel;
-    private JTextField cInputId;
-    private JLabel cPwLabel;
-    private JPasswordField cInputPw;
-    private JLabel cNameLabel;
-    private JTextField cInputName;
-    private JLabel cAgeLabel;
-    private JTextField cInputAge;
-    private JLabel cGenderLabel;
-    private JRadioButton cSelectMale;
-    private JRadioButton cSelectFemale;
-    private JLabel cPhoneLabel;
-    private JTextField cInputPhone;
-    private JLabel cAddrLabel;
-    private JButton createButton;
-    private JScrollPane AddrScroll;
-    private JTextArea cInputAddr;
-    private JDialog msgDialog;
-    private JPanel msgPanel;
-    private JLabel msgLabel;
-    private ButtonGroup genderBtnGroup;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }

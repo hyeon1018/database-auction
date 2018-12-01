ef2e1ac95c486147c706a6703417fc23ef422119
ro-chest-47 <45336226+ro-chest-47@users.noreply.github.com>
2018-12-01 22:43:18 +0900

    젠자아앙앙

--- src/main/java/com/db/auction/gui/Favorite.java
+++ src/main/java/com/db/auction/gui/Favorite.java
@@ -32,20 +32,18 @@ public class Favorite {
         favorList.setModel(model);
     }
 
-    private void addFavorBtnActionPerformed(ActionEvent e) {
-        // TODO 즐찾 유저 추가
-    }
-
     private void delFavorBtnActionPerformed(ActionEvent e) {
-        //favorList.getSelectedValue().toString();
-        // TODO 즐찾 유저 삭제
+        String target_user_id = (String) favorList.getSelectedValue();
+        Database.deleteFavorite(currentUser, target_user_id);
+        setList();
     }
 
     private void favorListMouseClicked(MouseEvent e) {
-        if(e.getClickCount() == 2){
-            //favorList.getSelectedValue().toString();
-
-            //TODO 검색 기능으로
+        String target_user_id = (String) favorList.getSelectedValue();
+        if(e.getClickCount() == 2) {
+            List <String []> favoriteResult = Database.FavoriteResultList(target_user_id);
+            new SearchResult(currentUser, favoriteResult);
+            favorFrame.dispose();
         }
     }
 
@@ -53,12 +51,11 @@ public class Favorite {
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-        // Generated using JFormDesigner Evaluation license - Yu Hwan Jung
+        // Generated using JFormDesigner Evaluation license - kangjungmo
         favorFrame = new JDialog();
         favorPanel = new JPanel();
         favorScroll = new JScrollPane();
         favorList = new JList();
-        addFavorBtn = new JButton();
         delFavorBtn = new JButton();
 
         //======== favorFrame ========
@@ -99,19 +96,12 @@ public class Favorite {
                 favorPanel.add(favorScroll);
                 favorScroll.setBounds(25, 20, 190, 175);
 
-                //---- addFavorBtn ----
-                addFavorBtn.setText("\ucd94\uac00");
-                addFavorBtn.setFont(new Font("\ub9d1\uc740 \uace0\ub515", Font.PLAIN, 12));
-                addFavorBtn.addActionListener(e -> addFavorBtnActionPerformed(e));
-                favorPanel.add(addFavorBtn);
-                addFavorBtn.setBounds(60, 210, 60, 25);
-
                 //---- delFavorBtn ----
                 delFavorBtn.setText("\uc0ad\uc81c");
                 delFavorBtn.setFont(new Font("\ub9d1\uc740 \uace0\ub515", Font.PLAIN, 12));
                 delFavorBtn.addActionListener(e -> delFavorBtnActionPerformed(e));
                 favorPanel.add(delFavorBtn);
-                delFavorBtn.setBounds(125, 210, 60, 25);
+                delFavorBtn.setBounds(155, 215, 60, 25);
             }
             favorFrameContentPane.add(favorPanel, BorderLayout.CENTER);
             favorFrame.setSize(245, 300);
@@ -121,12 +111,11 @@ public class Favorite {
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner Evaluation license - Yu Hwan Jung
+    // Generated using JFormDesigner Evaluation license - kangjungmo
     private JDialog favorFrame;
     private JPanel favorPanel;
     private JScrollPane favorScroll;
     private JList favorList;
-    private JButton addFavorBtn;
     private JButton delFavorBtn;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
